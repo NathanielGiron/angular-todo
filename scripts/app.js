e7d5ff1f81b76a6aa4b7e490b9a0fbc3e867a369
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-06-22 01:54:43 -0700

    some changes

--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,13 @@ angular.module("todoListApp", [])
   $scope.helloWorld = function() {
     console.log("Hello there! this is the helloWorld controller function, in the mainctrl");
   }
+
+  $scope.todos = [
+  	{"name": "clean the house"},
+  	{"name": "water the lawn"},
+  	{"name": "feed the dog"},
+  	{"name": "pay dem bills"},
+  	{"name": "run"},
+  	{"name": "swim"}
+  ]
 });
